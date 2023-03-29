12bdd59efd8df1fda098eb7031dc9df3a2fef109
DennisHvA <dennis.de.graaf2@hva.nl>
2023-03-29 15:27:53 +0200

    html form plus some needed js

--- eindopdracht/scripts/script.js
+++ eindopdracht/scripts/script.js
@@ -1,1 +1,28 @@
 console.log("linked")
+
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+      blah.src = URL.createObjectURL(file)
+    }
+  }
+
+// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const errorEmail = document.getElementById('error');
+
+email.removeAttribute("required")
+
+form.addEventListener('submit', (e) => {
+  let messages = [];
+  if (email.value === '' || email.value == null) {
+    messages.push('Email is required');
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorEmail.innerText = messages.join(', ');
+  }
+});
