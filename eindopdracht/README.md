03121e8838c38b2f56691bb4c8b81d7431568f46
DennisHvA <dennis.de.graaf2@hva.nl>
2023-03-30 18:27:02 +0200

    end of the day

---
+++ eindopdracht/README.md
@@ -0,0 +1,8 @@
+## Flow
+Features die niet werken
+
+- Camera aanzetten
+- Input type file laat geen naam zie
+- Input type color werkt niet. Hij laat alleen #fff zien en dit is niet te veranderen
+- Has selector werkt niet
+- Input type text kan je geen maxlenght meegeven
--- eindopdracht/scripts/script.js
+++ eindopdracht/scripts/script.js
@@ -7,8 +7,6 @@ imgInp.onchange = evt => {
     }
   }
 
-// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
-
 const form = document.querySelector('form');
 const email = document.getElementById('email');
 const errorEmail = document.getElementById('error');
@@ -35,4 +33,130 @@ form.text.addEventListener("keyup", myFunction);
 function myFunction() {
   let x = document.getElementById("text").value;
   text.innerHTML = x;
+}
+
+dragElement(document.getElementById("shirt"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+const red = document.querySelector("#red")
+const green = document.querySelector("#green")
+const blue = document.querySelector("#blue")
+const svg = document.querySelector("svg")
+
+red.addEventListener("click", () => {
+  svg.classList.add("red")
+  svg.classList.remove("green")
+  svg.classList.remove("blue")
+})
+
+green.addEventListener("click", () => {
+  svg.classList.add("green")
+  svg.classList.remove("red")
+  svg.classList.remove("blue")
+})
+
+blue.addEventListener("click", () => {
+  svg.classList.add("blue")
+  svg.classList.remove("green")
+  svg.classList.remove("red")
+})
+
+    // const scanner = document.querySelector("#scanner")
+    // const startbutton = document.querySelector('#start')
+    // const stopbutton = document.querySelector('#stop')
+    // if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    //     const constraints = { 
+    //     video: true,
+    //     audio: false
+    //     }
+
+    //     navigator.mediaDevices.getUserMedia({
+    //         audio: false, 
+    //         video: {
+    //             facingMode:'environment'
+    //         }
+    //     })
+    //     .then(stream => video.srcObject = stream);
+
+    //     scanner.classList.toggle("hide");
+    //     startbutton.classList.toggle("hide");
+    //     stopbutton.classList.toggle("hide");
+    //   }
+
+const cameraOn = document.querySelector('#start')
+const cameraOff = document.querySelector('#stop')
+
+cameraOn.addEventListener('click', enableCamera)
+cameraOff.addEventListener('click', disableCamera)
+
+function enableCamera() {
+  const scanner = document.querySelector("#scanner")
+  const startbutton = document.querySelector('#start')
+  const stopbutton = document.querySelector('#stop')
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const constraints = { 
+      video: true,
+      audio: false
+      }
+          
+      navigator.mediaDevices.getUserMedia({
+          audio: false, 
+          video: {
+              facingMode:'environment'
+          }
+      })
+      .then(stream => video.srcObject = stream);
+
+      scanner.classList.toggle("hide");
+      startbutton.classList.toggle("hide");
+      stopbutton.classList.toggle("hide");
+  }
+}
+
+
+function disableCamera() {
+  const scanner = document.querySelector("#scanner")
+  const startbutton = document.querySelector('#start')
+  const stopbutton = document.querySelector('#stop')
+  
+  const mediaStream = video.srcObject;
+  const tracks = mediaStream.getTracks();
+  tracks.forEach(track => track.stop())
+
+  scanner.classList.toggle("hide");
+  startbutton.classList.toggle("hide");
+  stopbutton.classList.toggle("hide");
 }
